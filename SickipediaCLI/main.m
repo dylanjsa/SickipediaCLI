4d0838ed46b7d67ad73a995ec0bad40d1e8ab0d1
Dylan Janeke <ash@Dylans-MacBook-Pro.local>
2012-03-23 16:01:27 +0200

    Initial Commit

---
+++ SickipediaCLI/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  SickipediaCLI
+//
+//  Created by Dylan Janeke on 2012/03/23.
+//  Copyright (c) 2012 AweSoftwe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
